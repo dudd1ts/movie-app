0c7affeea7d87227700e6cb06096053975f55b64
Sergey Kondrashev <kserg90@gmail.com>
2017-09-15 12:26:09 +0300

    Lazy-loading routes were added

--- src/router.js
+++ src/router.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Intro from './components/Intro.vue';
-import Movie from './components/Movie.vue';
-import MovieTrailer from './components/MovieTrailer.vue';
+// import Intro from './components/Intro.vue';
+const Intro = r => require.ensure([], () => r(require('./components/Intro.vue')));
+const Movie = r => require.ensure([], () => r(require('./components/Movie.vue')));
+const MovieTrailer = r => require.ensure([], () => r(require('./components/MovieTrailer.vue')));
+
+// import Movie from './components/Movie.vue';
+// import MovieTrailer from './components/MovieTrailer.vue';
 
 Vue.use(Router);
 
